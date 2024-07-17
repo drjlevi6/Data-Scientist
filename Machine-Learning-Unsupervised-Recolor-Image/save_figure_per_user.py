e5e0566537a7c6cb07412c3f09054b8c8ed41b72
drjlevi6 <drjlevi6@aol.com>
2024-07-17 14:08:10 -0400

    Add osascript commands to bring browser frontmost (Mac users only)

--- Machine-Learning-Unsupervised-Recolor-Image/save_figure_per_user.py
+++ Machine-Learning-Unsupervised-Recolor-Image/save_figure_per_user.py
@@ -4,9 +4,17 @@ import numpy as np
 from date_time_string import date_time_string
 from pandas import DataFrame as DF, Series as Ser
 import os
+import platform
 
 def save_figure_per_user(title="Image"):
 	'''This function must be called while user is doing a pyplot command'''
+	
+	# If computer' s Mac and browser is Chrome, activate browser first
+	if platform.system() == 'Darwin':
+		os.system("osascript  -e 'beep' 2>&1")
+		os.system("osascript -e 'tell application \"Chrome\" to activate' ; \
+			osascript -e 'delay 1'")
+		os.system("sleep 1")
 	if input("Save image? ('' = No, ~'' = Yes): ") != '':
 		savestring = title + '-' + date_time_string() + ".png"
 		plt.savefig(savestring)
