17a1ee19f814c8441dd1d02eb0bb1a7ca516fdcb
JONATHAN LEVI MD <drjlevi6@aol.com>
2024-12-10 16:16:05 -0500

    Renamed, deleted multiple files

--- transcriptScraperJXA/formatTranscript.bash
+++ transcriptScraperJXA/formatTranscript.bash
@@ -2,7 +2,16 @@
 
 # Format a LinkedIn course transcript
 
-selectTextLineNum=`pbpaste | sed -n '/Selecting transcript lines/='`
+# Locate text beginning
+#selectTextLineNum=$(( `pbpaste | sed -En '/ *\- \[/='` ))
+selectTextLineNum=$(( `pbpaste | sed -En '/^( *\- )?[\[\(]/=' | tail -n 1` ))
+echo $selectTextLineNum
 
-pbpaste | sed "1,$(( selectTextLineNum-2 ))d" | sed '2d' | fold -sw80 | \
-sed "$d" | pbcopy; pbpaste
+pbpaste | 
+sed "1,$(( selectTextLineNum - 3 ))d"  | # cut lines up to 2 before selectTextLineNum
+fold -sw80  | \
+sed '/Enable interactive transcripts/d; /Selecting transcript lines/d' |
+sed '/^video/d' | 
+sed '/^$/d' | # delete any blank lines (may be >1 at end)
+sed '/Find in transcript/d; /Filter results/d; /Chat with Coach/d' |
+pbcopy; pbpaste
