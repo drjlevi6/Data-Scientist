fd7d5364d17453dd804d628200ea4bcb1b94e4bd
JONATHAN LEVI MD <drjlevi6@aol.com>
2024-12-11 13:49:13 -0500

    Removed directory "ML-Unsupervised-Recolor-Image" (renamed)

--- ML-Unsupervised-Recolor-Image/rgb_to_hex.py
+++
@@ -1,4 +0,0 @@
-'''Convert triplet of RGB integers (0-255) to 6-digit hex string preceded by "#"'''
-def rgb_to_hex(rgb):
-	return f"#{hex(rgb[0])[2:]:0>2}{hex(rgb[1])[2:]:0>2}{hex(rgb[2])[2:]:0>2}"
-	
